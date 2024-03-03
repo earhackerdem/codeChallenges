8f564989ee2fa5184e167d0868158ce247cc89f9
Earvin Saúl Pérez Ramos <sehnhack@EarvinSamossAir.lan>
2024-03-03 16:24:43 -0600

    fc

--- hackRank/1ratios.php
+++ hackRank/1ratios.php
@@ -1,10 +1,5 @@
 <?php
 
-/*
- * Complete the 'plusMinus' function below.
- *
- * The function accepts INTEGER_ARRAY arr as parameter.
- */
 function plusMinus($arr) {
     $size = count($arr);
     $positive = 0;
@@ -26,10 +21,3 @@ function plusMinus($arr) {
 
 }
 
-$n = intval(trim(fgets(STDIN)));
-
-$arr_temp = rtrim(fgets(STDIN));
-
-$arr = array_map('intval', preg_split('/ /', $arr_temp, -1, PREG_SPLIT_NO_EMPTY));
-
-plusMinus($arr);
